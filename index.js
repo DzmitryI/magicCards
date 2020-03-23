3e20704b2f54d5012103d676d2371f304b2f7da5
Dzmitry Ivanou <3prn@mail.ru>
2020-03-23 10:34:17 +0300

    update gh-pages

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
+const api_base = `https://magiccards-70e57.firebaseio.com/MagicCard/-M35AbwPhyvrlnZUosLo.json`;
 const btn = document.querySelector('.btn');
-const image = document.querySelector('.card-img-top')
+const image = document.querySelector('.card-img-top');
 const arrImages = [
   'animpadistov_1.jpg',
   'animpadistov_2.jpg',
@@ -13,5 +14,32 @@ const arrImages = [
 btn.addEventListener('click', () => {
   const magicNumber = Math.floor(Math.random() * (arrImages.length));
   const src = `${image.src.slice(0, image.src.lastIndexOf('/') + 1)}${arrImages[magicNumber]}`;
-  image.src = src;
+  setSource({ src })
 });
+
+let timerId = setInterval(() => getSource(), 2000);
+
+setSource = async (body) => {
+  try {
+    const response = await fetch(`${api_base}`, {
+      method: 'PUT',
+      body: JSON.stringify(body),
+    });
+    await response.json();
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+getSource = async () => {
+  try {
+    const response = await fetch(`${api_base}`, {
+      method: 'GET',
+    });
+    const { src } = await response.json();
+    image.src = src;
+
+  } catch (error) {
+    console.log(error);
+  }
+};
